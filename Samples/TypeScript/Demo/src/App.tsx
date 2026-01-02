61577b457d4dc962a4cfd568d3508b593db8ebcb
thighnurger <lewis177777gmail.com>
2026-01-02 09:03:58 +0900

    ✨feat: 리액트쿼리

--- Samples/TypeScript/Demo/src/App.tsx
+++ Samples/TypeScript/Demo/src/App.tsx
@@ -1,11 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Router } from '@/pages/Router';
 
-const App = () => {
+const queryClient = new QueryClient();
 
+const App = () => {
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <Router />
-    </div>
+    </QueryClientProvider>
   );
 };
 
