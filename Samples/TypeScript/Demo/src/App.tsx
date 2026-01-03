0ce1ef1cb8111f27ae0afb1c2bf7cdf51211a690
thighnurger <lewis177777gmail.com>
2026-01-04 00:05:28 +0900

    🐛 fix: 머지충돌해결

--- Samples/TypeScript/Demo/src/App.tsx
+++ Samples/TypeScript/Demo/src/App.tsx
@@ -1,14 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Router } from '@/pages/Router';
+import { GlobalStyles } from '@/style/GlobalStyles';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
-      <QueryClientProvider client={queryClient}>
-        <Router />
-      </QueryClientProvider>
-    </>
+      <Router />
+    </QueryClientProvider>
   );
 }
 
